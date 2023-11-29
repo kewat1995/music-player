8894086bb8c76cf64de212c6c6dce5cfa47d7dfd
kewat1995 <surendra121030@gmail.com>
2023-11-29 16:03:43 +0530

    done music player

---
+++ index.js
@@ -0,0 +1,32 @@
+const progress = document.getElementById("progress");
+const song = document.getElementById("song");
+const controlIcon = document.getElementById("control-icon");
+
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+  
+};
+
+function playPause() {
+  if (controlIcon.classList.contains("fa-pause")) {
+    song.pause();
+    controlIcon.classList.remove("fa-pause");
+    controlIcon.classList.add("fa-play");
+  } else {
+    song.play();
+    controlIcon.classList.add("fa-pause");
+    controlIcon.classList.remove("fa-play");
+  }
+}
+
+if (song.play) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+}
+progress.onchange = function () {
+  song.currentTime = progress.value;
+  controlIcon.classList.add("fa-pause");
+  controlIcon.classList.remove("fa-play");
+};
